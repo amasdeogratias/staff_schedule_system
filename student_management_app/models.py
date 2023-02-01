f48bcd7a467fdff88ea1a7ee39bcae5bf34bb38f
amasdeogratias <deogratiasprog@gmail.com>
2023-02-01 15:40:52 +0300

    added course model

--- student_management_app/models.py
+++ student_management_app/models.py
@@ -19,5 +19,13 @@ class Staffs(models.Model):
     address=models.TextField()
     created_at=models.DateTimeField(auto_now_add=True)
     updated_at=models.DateTimeField(auto_now_add=True)
+    objects=models.Manager()
+    
+class Courses(models.Model):
+    id=models.AutoField(primary_key=True)
+    course_name=models.CharField(max_length=255)
+    course_code=models.CharField(max_length=255)
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now_add=True)
     objects=models.Manager()    
 
