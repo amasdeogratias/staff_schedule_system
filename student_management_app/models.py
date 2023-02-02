5a2fcd769195ca85c492b25a838e552e59ce5e32
amasdeogratias <deogratiasprog@gmail.com>
2023-02-02 16:58:21 +0300

    removed usertypes in database logic

--- student_management_app/models.py
+++ student_management_app/models.py
@@ -2,20 +2,20 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
-class CustomUser(AbstractUser):
-    user_type_data=((1,"HOD"),(2,"Staff"),(3,"Student"))
-    user_type=models.CharField(default=1,choices=user_type_data,max_length=10)
+# class CustomUser(AbstractUser):
+#     user_type_data=((1,"HOD"),(2,"Staff"),(3,"Student"))
+#     user_type=models.CharField(default=1,choices=user_type_data,max_length=10)
     
 class AdminHOD(models.Model):
     id=models.AutoField(primary_key=True)
-    admin=models.OneToOneField(CustomUser,on_delete=models.CASCADE)
+    # admin=models.OneToOneField(CustomUser,on_delete=models.CASCADE)
     created_at=models.DateTimeField(auto_now_add=True)
     updated_at=models.DateTimeField(auto_now_add=True)
     objects=models.Manager()
     
 class Staffs(models.Model):
     id=models.AutoField(primary_key=True)
-    admin=models.OneToOneField(CustomUser,on_delete=models.CASCADE)
+    # admin=models.OneToOneField(CustomUser,on_delete=models.CASCADE)
     address=models.TextField()
     created_at=models.DateTimeField(auto_now_add=True)
     updated_at=models.DateTimeField(auto_now_add=True)
@@ -24,7 +24,7 @@ class Staffs(models.Model):
 class Department(models.Model):
     id=models.AutoField(primary_key=True)
     department_name=models.CharField(max_length=255)
-    staff_id=models.ForeignKey(CustomUser,on_delete=models.CASCADE)
+    # staff_id=models.ForeignKey(CustomUser,on_delete=models.CASCADE)
     created_at=models.DateTimeField(auto_now_add=True)
     updated_at=models.DateTimeField(auto_now_add=True)
     objects=models.Manager()   
