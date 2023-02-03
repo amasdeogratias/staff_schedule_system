c3d1bf1287c088fa1e4cf51d18b0ace01e810c9e
amasdeogratias <deogratiasprog@gmail.com>
2023-02-03 14:21:23 +0300

    moved templates to the app

--- student_management_app/views.py
+++ student_management_app/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def showDemoPage(request):
-    return render(request, 'demo.html')
+    return render(request, 'student_management_app/demo.html')
