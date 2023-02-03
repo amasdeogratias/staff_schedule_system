a858bc8c0440a4573b2f007301d9c0c12a44c538
amasdeogratias <deogratiasprog@gmail.com>
2023-02-03 15:38:30 +0300

    added students model

--- student_management_app/models.py
+++ student_management_app/models.py
@@ -37,4 +37,18 @@ class Courses(models.Model):
     created_at=models.DateTimeField(auto_now_add=True)
     updated_at=models.DateTimeField(auto_now_add=True)
     objects=models.Manager() 
+    
+class Students(models.Model):
+    id=models.AutoField(primary_key=True)
+    # admin=models.OneToOneField(CustomUser,on_delete=models.CASCADE)
+    student_id=models.CharField(max_length=255)
+    gender=models.CharField(max_length=255)
+    profile_pic=models.FileField()
+    address=models.TextField()
+    course_id=models.ForeignKey(Courses,on_delete=models.DO_NOTHING)
+    session_start_year=models.DateField()
+    session_end_year=models.DateField()
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now_add=True)
+    objects = models.Manager()
 
