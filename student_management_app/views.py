f53ea88bca564833cdd51f84bfb0ee2dca679272
amasdeogratias <deogratiasprog@gmail.com>
2023-02-07 16:44:15 +0300

    function to display login page

--- student_management_app/views.py
+++ student_management_app/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def showDemoPage(request):
     return render(request, 'student_management_app/demo.html')
+    
+def showLoginPage(request):
+    return render(request, 'student_management_app/login.html')
