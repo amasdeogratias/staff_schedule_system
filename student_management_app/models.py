7a2f98bf349e93fb2e915e308defdbb711ebb91a
amasdeogratias <deogratiasprog@gmail.com>
2023-02-07 12:39:10 +0300

    add signals for adding usertypes when new user is created

--- student_management_app/models.py
+++ student_management_app/models.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from  django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
 class CustomUser(AbstractUser):
@@ -41,7 +43,7 @@ class Courses(models.Model):
 class Students(models.Model):
     id=models.AutoField(primary_key=True)
     admin=models.OneToOneField(CustomUser,on_delete=models.CASCADE)
-    student_id=models.CharField(max_length=255)
+    student_id=models.CharField(max_length=255, unique=True, blank=False)
     gender=models.CharField(max_length=255)
     profile_pic=models.FileField()
     address=models.TextField()
@@ -51,4 +53,14 @@ class Students(models.Model):
     created_at=models.DateTimeField(auto_now_add=True)
     updated_at=models.DateTimeField(auto_now_add=True)
     objects = models.Manager()
+    
+@receiver(post_save,sender=CustomUser)
+def create_user_profile(sender,instance,created,**kwargs):
+    if created:
+        if instance.user_type == 1:
+            AdminHOD.objects.create(admin=instance)
+        if instance.user_type == 2:
+            Staffs.objects.create(admin=instance)
+        if instance.user_type == 3:
+            Students.objects.create(admin=instance)
 
