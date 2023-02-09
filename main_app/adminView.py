28ed389f8bc1fb6674cc1ad4119bf4aaaf4ce77c
amasdeogratias <deogratiasprog@gmail.com>
2023-02-09 15:48:23 +0300

    added function for submiting staff in the database

--- main_app/adminView.py
+++ main_app/adminView.py
@@ -1,4 +1,8 @@
+from django.http import HttpResponse,HttpResponseRedirect
+from django.contrib import messages
 from django.shortcuts import render
+from .models import *
+from django.urls import reverse
 
 
 #
@@ -7,3 +11,28 @@ def admin_home(request):
     
 def add_staff(request):
     return render(request, 'main_app/admin/add_staff.html')
+    
+def add_staff_save(request):
+    if request.method != 'POST':
+        return HttpResponse('Method not allowed')
+    else: # process the form
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        address = request.POST.get('address')
+        
+        #create custom user object
+        try:
+            user = CustomUser.objects.create_user(
+               username=username,email=email,password=password,first_name=first_name,last_name=last_name, user_type=2)
+            user.staffs.address=address
+            user.save()
+            messages.success(request, "Staff added successfully...")
+            return HttpResponseRedirect(reverse("add_staff"))
+            
+        except:
+            messages.error(request, "Problem in Staff creation...")
+            return HttpResponseRedirect(reverse("add_staff"))
+            
--- main_app/urls.py
+++ main_app/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     path('user_details', views.getUserDetails),
     path('logout_user', views.logout_user, name='logout'),
     path('admin_dashboard', adminView.admin_home, name='admin_dashboard'),
-    path('add_staff', adminView.add_staff, name='add_staff')
+    path('add_staff', adminView.add_staff, name='add_staff'),
+    path('add_staff_save', adminView.add_staff_save)
 ]
