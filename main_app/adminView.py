9b1e0037a0cf18b7f081456c4e1811ac5aee426d
amasdeogratias <deogratiasprog@gmail.com>
2023-02-11 13:03:11 +0300

    function to create student

--- main_app/adminView.py
+++ main_app/adminView.py
@@ -88,5 +88,34 @@ def add_student(request):
     return render(request, 'main_app/admin/add_student.html')
     
 def add_student_save(request):
-    return render(request, 'main_app/admin/add_student.html')
+    if request.method != 'POST':
+        return HttpResponse("<h2>Method Not Allowed</h2>")
+    else: # process the form
+        student_id = request.POST.get('student_id')
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        program = request.POST.get('program') 
+        gender = request.POST.get('gender') 
+        session_start = request.POST.get('session_start') 
+        session_end = request.POST.get('session_end') 
+        
+        #create custom user object
+        try:
+            user = CustomUser.objects.create_user(
+               username=username,email=email,password=password,first_name=first_name,last_name=last_name, user_type=3)
+            user.students.student_id=student_id
+            user.students.program=program
+            user.students.gender=gender
+            user.students.session_start_year=session_start
+            user.students.session_end_year=session_end
+            user.save()
+            messages.success(request, "Student added successfully...")
+            return HttpResponseRedirect(reverse("add_student"))
+            
+        except:
+            messages.error(request,'Failed to add student')
+            return HttpResponseRedirect(reverse("add_student"))
             
