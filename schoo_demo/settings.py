adc879889f0d43cc455db1b980608b85a9942df0
amasdeogratias <deogratiasprog@gmail.com>
2023-02-13 15:05:02 +0300

    configured middleware class in the project settings

--- schoo_demo/settings.py
+++ schoo_demo/settings.py
@@ -56,6 +56,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'main_app.LoginCheckMiddleWare.LoginCheckMiddleWare',
 ]
 
 ROOT_URLCONF = 'schoo_demo.urls'
