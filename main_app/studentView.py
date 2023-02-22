6627b8264c9483e483687db18e505b1f401b6972
amasdeogratias <deogratiasprog@gmail.com>
2023-02-22 14:57:31 +0300

    view available staffs by student

--- main_app/studentView.py
+++ main_app/studentView.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
+from .models import Staffs
 
 #
 def student_panel(request):
     return render(request,"main_app/students/student_panel.html")
 
 def view_lectures(request):
-    return render(request, 'main_app/students/lectures.html')
+    staffs = Staffs.objects.all()
+    context = {'staffs':staffs}
+    return render(request, 'main_app/students/lectures.html',context)
 
 def add_appointment(request):
     return render(request,'main_app/students/add_booking.html')
