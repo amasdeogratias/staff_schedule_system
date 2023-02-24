e853ec0144bb64ef638cc5e43bdf810c698b67f9
amasdeogratias <deogratiasprog@gmail.com>
2023-02-24 15:09:28 +0300

    get json data for time slots based on date selected

--- main_app/studentView.py
+++ main_app/studentView.py
@@ -1,5 +1,8 @@
+import json
 from django.shortcuts import render
-from .models import Staffs
+from django.http import HttpResponse, JsonResponse, HttpResponseRedirect
+from .models import Staffs, TimeSlot
+from django.views.decorators.csrf import csrf_exempt
 
 #
 def student_panel(request):
@@ -14,3 +17,15 @@ def add_appointment(request,staff_id):
     staff=Staffs.objects.get(admin=staff_id)
     context = {'staff':staff}
     return render(request,'main_app/students/add_booking.html',context)
+
+@csrf_exempt
+def get_time_slots(request):
+    staff_id = request.POST.get("staff_id")
+    booking_date = request.POST.get("booking_date")
+    time_slots = TimeSlot.objects.filter(slot_date=booking_date, staff=staff_id)
+    list_data = []
+    
+    for slot in time_slots:
+        data_small = {"time":slot.time}
+        list_data.append(data_small)
+    return JsonResponse(json.dumps(list_data),content_type="application/json",safe=False)
