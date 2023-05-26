3ef74302dc5f46916bb148cd882de080934fcf2e
amasdeogratias <deogratiasprog@gmail.com>
2023-05-26 11:58:52 +0300

    created form for confirm password

--- schoo_demo/urls.py
+++ schoo_demo/urls.py
@@ -28,7 +28,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('reset_password/', PasswordResetView.as_view(template_name='main_app/users/password_reset.html'), name='reset_password'),
     path('reset_password_sent/', PasswordResetDoneView.as_view(template_name='main_app/users/password_reset_done.html'), name='password_reset_done'),
-    path('reset/<uidb64>/<token>/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/<uidb64>/<token>/', PasswordResetConfirmView.as_view(template_name='main_app/users/password_reset_confirm.html'), name='password_reset_confirm'),
     path('reset_password_complete/', PasswordResetCompleteView.as_view(), name='password_reset_complete'),
     path('', include('main_app.urls')),
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
